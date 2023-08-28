be631fc5ba32d5ff91257c9734b9662d35d8acb4
Tanjil Ahamed <tanjil113355@gmail.com>
2023-08-28 20:24:24 +0600

    javascript section complete

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const loadData = async (inputText, isShowAll) => {
+const loadData = async (inputText = 'iphone', isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${inputText}`)
     const data = await res.json();
     const PhoneData = data.data;
@@ -16,7 +16,7 @@ const displayData = (phones, showAll) => {
     } else {
         showMore.classList.add('hidden');
     }
-    if(!showAll){
+    if (!showAll) {
         phones = phones.slice(0, 12);
     }
     phones.forEach(phone => {
@@ -26,11 +26,12 @@ const displayData = (phones, showAll) => {
         phoneCard.innerHTML = `
        <div class="">
           <figure><img src="${phone.image}" alt="Shoes" /></figure>
-          <div class="card-body">
-             <h2 class="card-title">${phone.phone_name}</h2>
-             <p>If a dog chews shoes whose shoes does he choose?</p>
-           <div class="card-actions">
-              <button class="btn btn-primary">Buy Now</button>
+          <div class="card-body text-center">
+             <h2 class="text-2xl font-medium">${phone.phone_name}</h2>
+             <p>There are many variations of passages of available, but the majority have suffered</p>
+             <h3 class="text-xl font-medium">999$</h3>
+           <div class="card-actions justify-center mt-3">
+              <button onclick="showDetails('${phone.slug}');my_modal_5.showModal()"  class="btn btn-primary">Show Details</button>
             </div>
          </div>
        </div>
@@ -39,6 +40,43 @@ const displayData = (phones, showAll) => {
     })
     loading(false);
 };
+
+
+// details
+const showDetails = async (id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+    const data = await res.json()
+    const details = data.data;
+    // console.log(console.log(details));
+    displayDetails(details)
+};
+
+const displayDetails = (data) => {
+    console.log(data);
+    const phoneDetailContainer = document.getElementById('phone-detail-container');
+    phoneDetailContainer.innerHTML = `
+    <div class="bg-gray-200 px-20 py-10 rounded-lg items-center text-center flex mx-auto justify-center">
+    <img src="${data.image}" alt="">
+    </div>
+    <h3 class="font-semibold text-2xl mt-8">${data.name}</h3>
+    <p class="py-4">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+    <h3 class="font-bold text-xl">Storage : <span class="text-[#706F6F] font-normal">${data.mainFeatures.storage}</span></h3>
+    <h3 class="font-bold text-xl text-[#403F3F]">Display Size</span> : <span class="text-[#706F6F] font-normal">${data.mainFeatures.displaySize}</span></h3>
+    <h3 class="font-bold text-xl text-[#403F3F]">chipSet</span> : <span class="text-[#706F6F] font-normal">${data.mainFeatures.chipSet}</span></h3>
+    <h3 class="font-bold text-xl text-[#403F3F]">Memory</span> : <span class="text-[#706F6F] font-normal">${data.mainFeatures.memory}</span></h3>
+    <h3 class="font-bold text-xl text-[#403F3F]">slug</span> : <span class="text-[#706F6F] font-normal">${data.slug}</span></h3>
+    <h3 class="font-bold text-xl text-[#403F3F]">release Date : <span class="text-[#706F6F] font-normal">${data.releaseDate}</span></h3>
+    <h3 class="font-bold text-xl text-[#403F3F]">Brand : <span class="text-[#706F6F] font-normal">${data.brand}</span></h3>
+    <h3 class="font-bold text-xl text-[#403F3F] ">GPS : <span class="text-[#706F6F] font-normal">${data.others.GPS}</span></h3>
+    
+     <div class="modal-action ">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn bg-red-400 text-white">Close</button>
+    </div>
+    `
+
+}
+
 
 const phoneSearch = (isShowAll) => {
     const inputFiled = document.getElementById('input-filed');
@@ -60,3 +98,6 @@ const loading = (isLoading) => {
 const showAll = () => {
     phoneSearch(true);
 }
+
+
+loadData();
